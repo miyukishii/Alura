bc469e045ed9197fef9ef6f2760aa0e985390295
Miyuki Ishii <miyukisabrina@gmail.com>
2022-12-14 20:57:42 -0300

    Parte 3 em andamento sobre react navegation

---
+++ React Native/Parte3/anotacoes.js
@@ -0,0 +1,17 @@
+// Aplicaçao de navegação entre telas.
+// O que é navegação e sua importância:
+// Vimos que a navegação é a funcionalidade em que o usuário pode ir (navegar) de uma tela a outra. No desenvolvimento mobile, precisamos criar as telas e a navegação entre essas telas. Também aprendemos que existem formas diferentes de implementar a navegação em um aplicativo. A navegação é importante, pois permite que o usuário navegue de uma tela para outra e não fique preso em uma única tela, por exemplo.
+
+// A Aplicação conterá as seguintes telas:
+// Home - com lista de produtores;
+// Cesta
+// Carrinho
+
+// Existem 4 tipos de navegação em apps
+// 1 - armazena as telas que vão passando, podendo retornar ( Empilhada ou stack)
+// 2 - As telas vão passando e não se sabe qqual a tela anterior(Aba ou tab)
+// 3 - menu que expande opções para trocar de tela (menu lateral - drawer)
+// 4 - acessas telas por meio de url (deep link)
+
+// Vamos utilizar a biblioteca React Navigation
+
