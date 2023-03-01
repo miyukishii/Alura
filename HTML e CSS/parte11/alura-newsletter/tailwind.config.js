43c7dd54022efb602a7266bfc5f01b5ffa6029e9
Miyuki Ishii <miyukisabrina@gmail.com>
2023-03-01 15:49:59 -0300

    parte da aula 3

---
+++ HTML e CSS/parte11/alura-newsletter/tailwind.config.js
@@ -0,0 +1,16 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                blue: {
+                    light: '#C5DFFF',
+                    dark: '#061E3C',
+                    hover: '#1057B0',
+                },
+            },
+            fontFamily: {
+                inter: ['Inter', 'sans-serif'],
+            }
+        }
+    }
+}
