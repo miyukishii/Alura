c672c88f2900112c70e002946e569ce09b958e4b
Miyuki Ishii <miyukisabrina@gmail.com>
2023-03-03 13:33:34 -0300

    aula 1 finalizada

--- Next/parte1/pages/_app.js
+++ Next/parte1/pages/_app.js
@@ -1,6 +1,4 @@
-import Head from "next/head";
 import GlobalStyle from "../src/theme/GlobalStyle";
-import Document from "./_document";
 
 export default function MyApp({ Component, pageProps }) {
     return (
--- Next/parte1/pages/faq.js
+++ Next/parte1/pages/faq.js
@@ -1,28 +1,9 @@
+import FAQScreen from "../src/screens/FAQScreen";
 
-import Link from '../src/components/Link';
-import PageTitle from '../src/components/PageTitle';
+export default FAQScreen
 
 // Utilizado para fazer sites estáticos. Roda somente em build time. Sempre vai ter a resposta mais rápida pro usuário.
-// export async function getStaticProps() {
-//     const FAQ_API_URL = 'https://gist.githubusercontent.com/omariosouto/0ceab54bdd8182cbd1a4549d32945c1a/raw/578ad1e8e5296fa048e3e7ff6b317f7497b31ad9/alura-cases-faq.json ';
-//     const faq = await fetch(FAQ_API_URL)
-//       .then((response) => {
-//         return response.json();
-//       })
-//       .then((data) => {
-//         return data;
-//       });
-
-//     return {
-//       props: {
-//         // Qualquer coisa passada aqui, será passada como props no componente de baixo
-//         faq
-//       },
-//     }
-// }
-
-// Roda a cada acesso que você recebe. Caso precise de coisas dinâmicas.
-export async function getServerSideProps() {
+export async function getStaticProps() {
     const FAQ_API_URL = 'https://gist.githubusercontent.com/omariosouto/0ceab54bdd8182cbd1a4549d32945c1a/raw/578ad1e8e5296fa048e3e7ff6b317f7497b31ad9/alura-cases-faq.json ';
     const faq = await fetch(FAQ_API_URL)
       .then((response) => {
@@ -40,27 +21,46 @@ export async function getServerSideProps() {
     }
 }
 
-export default function FAQPage({faq}) {
-    return (
-        <div>
-            <PageTitle>
-                FAQ - Alura Cases Campanha
-            </PageTitle>
-            <h1>Alura Cases - Página de Pesguntas FAQ</h1>
-            <Link href="/">
-              Ir para Home
-            </Link>
+// Roda a cada acesso que você recebe. Caso precise de coisas dinâmicas.
+// export async function getServerSideProps() {
+//     const FAQ_API_URL = 'https://gist.githubusercontent.com/omariosouto/0ceab54bdd8182cbd1a4549d32945c1a/raw/578ad1e8e5296fa048e3e7ff6b317f7497b31ad9/alura-cases-faq.json ';
+//     const faq = await fetch(FAQ_API_URL)
+//       .then((response) => {
+//         return response.json();
+//       })
+//       .then((data) => {
+//         return data;
+//       });
 
-            <ul>
-                {faq.map(({answer, question}, index) => (
-                    <li key={index}>
-                        <article>
-                            <h2>{question}</h2>
-                            <p>{answer}</p>
-                        </article>
-                    </li>
-                ))}
-            </ul>
-        </div>
-    )
-}
+//     return {
+//       props: {
+//         // Qualquer coisa passada aqui, será passada como props no componente de baixo
+//         faq
+//       },
+//     }
+// }
+
+// export default function FAQPage({faq}) {
+//     return (
+//         <div>
+//             <PageTitle>
+//                 FAQ - Alura Cases Campanha
+//             </PageTitle>
+//             <h1>Alura Cases - Página de Pesguntas FAQ</h1>
+//             <Link href="/">
+//               Ir para Home
+//             </Link>
+
+//             <ul>
+//                 {faq.map(({answer, question}, index) => (
+//                     <li key={index}>
+//                         <article>
+//                             <h2>{question}</h2>
+//                             <p>{answer}</p>
+//                         </article>
+//                     </li>
+//                 ))}
+//             </ul>
+//         </div>
+//     )
+// }
